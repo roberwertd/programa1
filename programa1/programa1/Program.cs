d38df397934706e3094af04299bdafd07979916b
MariadelRosario123 <Rosa.linda_101@hotmail.com>
2014-09-25 19:51:31 -0500

    mi  primer programa

---
+++ programa1/programa1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 25/09/2014
+ * Time: 07:50 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace programa1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
